44d04b8e9ff93a1923847cb3b6ed60c62140c0ad
Chrisfian06062003 <krisfianbeniandriano@gmail.com>
2024-10-09 18:45:40 +0700

    Add files via upload

---
+++ latihan kriptografi.py
@@ -0,0 +1,23 @@
+def encrypt(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+
+    return result
+
+
+def decrypt(text, shift):
+    return encrypt(text, -shift)
+
+
+
+text = input("Masukkan teks: ")
+shift = int(input("Masukkan nilai pergeseran (shift): "))
+print("Teks terenkripsi: ", encrypt(text, shift))
+print("Teks terdekripsi: ", decrypt(text, shift))
